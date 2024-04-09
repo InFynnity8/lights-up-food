38abeba9098dfd816c60d2883aa483dc8dec281f
ghost <inFynnity8>
2024-04-09 14:07:06 +0000

    yes

--- backend/main.js
+++ backend/main.js
@@ -1,30 +1,58 @@
-var nodemailer = require('nodemailer');
+const nodemailer = require('nodemailer');
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.post("/", (req, res) => {
+
+})
+
+app.post("/failure", (req, res) => {
+  res.redirect("/")
+})
+
+app.post("/success", (req, res) => {
+  res.redirect("/")
+})
+
+app.listen(3000, () => {
+  console.log("server started on port 3000")
+});
+
+
+
+
+
+
+
+
 
 
 const main = async () => {
-
       const transporter = nodemailer.createTransport({
         service: 'gmail',
         host: "smtp.gmail.com",
-        port: 587,  //465
-        secure: false, 
+        port: 465,
+        secure: true, 
         auth: {
           user: 'sackeyemmanuelfynn@gmail.com',
-          pass: '@Daddybosco3'
+          pass: 'pagazndczdviclnc'
         }
       });
 
       const mailOptions = {
         from: {
-          name: 'daddybosco3',
+          name: 'Fynn',
           address: 'sackeyemmanuelfynn@gmail.com'
         },
         to: 'daddybosco3@gmail.com',
-        subject: 'Sending Email using Node.js',
+        subject: 'Lights Up Orders',
         text: 'That was easy!'
       };
 
-      const info = await transporter.sendMail(mailOptions, function(error, info){
+      const info = await transporter.sendMail(mailOptions, (error, info) => {
           if (error) {
             console.log(error);
           } else {
